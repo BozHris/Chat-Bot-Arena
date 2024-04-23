45d813c11f720fc82f0f2d1702a97dbdab194e6a
BozHris <bozidarhristovski@gmail.com>
2024-04-23 15:00:06 +0200

    Removed sensitive files

--- env.py
+++
@@ -1,3 +0,0 @@
-
-Open_AI_APIKEY = "sk-proj-OjHPjDxiPh63YzppNkV7T3BlbkFJphEOE3AkNponscltfOUO"
-Claude_API_KEY = "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"
